b46458470e94b5da493d7e205c6a34663c226f96
Mohan Das <mohandaskj.11@gmail.com>
2026-06-15 16:26:09 +0530

    Add function to plot distance vs speed for drivers
    - Introduced a new function  to visualize the relationship between distance and speed for multiple drivers.
    - The function takes in a list of driver data and their corresponding names, plotting each driver's distance against their speed.
    - Enhanced the plotting capabilities of the Sliplib library for better data analysis.

--- Sliplib/plotting.py
+++ Sliplib/plotting.py
@@ -15,3 +15,16 @@ def plot_race_progression(drivers):
     plt.title(f"Race Progression of {len(drivers)} drivers")
     plt.legend()
     plt.show()
+
+def plot_dist_speed(drivers,names):
+    plt.figure(figsize=(8,6))
+    for i,dr in enumerate(drivers):
+        x = dr["Distance"]
+        y = dr["Speed"]
+
+        plt.plot(x,y,label=names[i])
+    plt.xlabel("Distance")
+    plt.ylabel("Speed")
+    plt.title(f"Distance vs Speed of {len(drivers)} Drivers")
+    plt.legend()
+    plt.show()
