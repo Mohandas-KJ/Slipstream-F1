6a3bc488761d5403e141a8078502164f266b2273
Mohan Das <mohandaskj.11@gmail.com>
2026-06-23 10:00:54 +0530

    Add additional plotting functions for driver performance metrics
    - Implemented plot_dist_throttle to visualize throttle application over distance for each driver.
    - Implemented plot_dist_ngear to visualize gear usage over distance for each driver.
    - Fixed typo in plot_dist_brake function label from \Bake\ to \Brake\.
    - Enhanced plot_dist_brake to include proper labeling and visualization for brake application.

--- Sliplib/plotting.py
+++ Sliplib/plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# Plot Driver Position given the laps
 def plot_race_progression(drivers):
     plt.figure(figsize=(8,6))
     for dr in drivers:
@@ -16,6 +17,7 @@ def plot_race_progression(drivers):
     plt.legend()
     plt.show()
 
+# Plot Speed of drivers given the distance for a single lap (fastest lap)
 def plot_dist_speed(drivers,names):
     plt.figure(figsize=(8,6))
     for i,dr in enumerate(drivers):
@@ -29,6 +31,7 @@ def plot_dist_speed(drivers,names):
     plt.legend()
     plt.show()
 
+# Plot Brake Application of drivers given the distance for a single lap (fastest lap)
 def plot_dist_brake(drivers,names):
     plt.figure(figsize=(8,6))
     for i,dr in enumerate(drivers):
@@ -37,7 +40,35 @@ def plot_dist_brake(drivers,names):
 
         plt.plot(x,y,label=names[i])
     plt.xlabel("Distance")
-    plt.ylabel("Bake")
+    plt.ylabel("Brake")
     plt.title(f"Distance vs Brake of {len(drivers)} Drivers")
+    plt.legend()
+    plt.show()
+
+# Plot Throttle of drivers given the distance for a single lap (fastest lap)
+def plot_dist_throttle(drivers,names):
+    plt.figure(figsize=(8,6))
+    for i,dr in enumerate(drivers):
+        x = dr["Distance"]
+        y = dr["Throttle"]
+
+        plt.plot(x,y,label=names[i])
+    plt.xlabel("Distance")
+    plt.ylabel("Throttle")
+    plt.title(f"Distance vs Throttle of {len(drivers)} Drivers")
+    plt.legend()
+    plt.show()
+
+# Plot nGears of drivers given the distance for a single lap (fastest lap)
+def plot_dist_ngear(drivers,names):
+    plt.figure(figsize=(8,6))
+    for i,dr in enumerate(drivers):
+        x = dr["Distance"]
+        y = dr["nGear"]
+
+        plt.plot(x,y,label=names[i])
+    plt.xlabel("Distance")
+    plt.ylabel("nGear")
+    plt.title(f"Distance vs nGear of {len(drivers)} Drivers")
     plt.legend()
     plt.show()
