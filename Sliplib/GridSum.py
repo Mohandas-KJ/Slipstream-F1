8d1f65460e99ea4403ba4fda18bc417acfa4303d
Mohan Das <mohandaskj.11@gmail.com>
2026-06-23 21:51:23 +0530

    Add function to calculate position gain for drivers
    - Implemented  function to compute the total position gain for each driver based on their race positions.
    - The function aggregates position changes and returns a sorted DataFrame of drivers by their position gain.

--- Sliplib/GridSum.py
+++ Sliplib/GridSum.py
@@ -18,3 +18,25 @@ def get_stint(data):
 
     return df.sort_values(by="Pit Stops",ascending=True).reset_index(drop=True)
 
+def calc_position_gain(data):
+
+    pos = {}
+
+    drivers = data["Driver"].unique()
+
+    for i in drivers:
+        gain_meter = 0
+        pos_list = data[data["Driver"] == i]["Position"].dropna().tolist()
+
+        c = pos_list[0]
+
+        for p in pos_list[1:]:
+            addi = c - p
+            c = p
+            gain_meter += addi
+        
+        pos[i] = int(gain_meter)
+
+    df = pd.DataFrame(pos.items(),columns=["Driver","Position Gain"])
+
+    return df.sort_values(by="Position Gain",ascending=False).reset_index(drop=True) 
