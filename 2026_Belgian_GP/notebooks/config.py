9fc317a3f0ade533de9925a57372e27c1b60ad2e
Mohan Das <mohandaskj.11@gmail.com>
2026-07-14 21:10:54 +0530

    Add function to write contenders to configuration file
    - Implemented  function to write the list of contenders to the configuration file.
    - The function opens the configuration file in write mode and saves the contenders in a formatted string.
    - Added a print statement to confirm that the configuration has been written successfully.

---
+++ 2026_Belgian_GP/notebooks/config.py
@@ -0,0 +1,1 @@
+CONTENDERS = ['HAM', 'ANT', 'VER', 'LEC']
--- configuration/writer.py
+++ configuration/writer.py
@@ -1,1 +1,9 @@
 # Funtion to write Contenders 
+def write_contenders(Contenders):
+    
+    # Write to file
+    with open("../configuration/config.py","w") as conf:
+        conf.write(f"CONTENDERS = {repr(Contenders)}\n")
+    
+    # print
+    print("Configuration Written!") 
